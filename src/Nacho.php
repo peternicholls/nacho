018f754d3f5bbb548b0574a938f277bf2c11c282
Peter Nicholls <nicholls@mac.com>
2017-03-08 07:59:37 +0000

    added class and phpunit xml

---
+++ src/Nacho.php
@@ -0,0 +1,15 @@
+<?php namespace PeterNicholls\Nacho;
+/**
+ * Created by PhpStorm.
+ * User: peternicholls
+ * Date: 08/03/2017
+ * Time: 07:54
+ */
+
+class Nacho {
+
+    public function hasCheese($bool = true)
+    {
+        return $bool;
+    }
+}
